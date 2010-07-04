8ccba5a58004ca3035ecb75e4d86fc87359620d3
Marcin <marcin.nowak@sonyx.net>
2010-07-04 10:58:40 +0200

    scaffolding for projects support added

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
