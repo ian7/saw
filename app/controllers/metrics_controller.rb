15262e95683832ce47d9e29fdd8d8352221659e3
Marcin Nowak <marcinnowak@marcin-nowaks-macbook-pro.local>
2010-07-07 18:19:49 +0200

    very basic metrics implementation added - see master branch for more details

---
+++ app/controllers/metrics_controller.rb
@@ -0,0 +1,39 @@
+class MetricsController < ApplicationController
+  def choice
+  end
+
+  def descriptiveness
+    
+    # in case there is only one node specified, that might render some nice node-centric vis
+    id = params[:id]  
+    @descriptiveness_level = 0
+    if id != nil
+      t=Taggable.find id
+      @descriptiveness_level = t.tags.count
+    end
+    
+    # in case there is node list, then return json
+    
+    nodes = params[:nodes]
+    codec = ActiveSupport::JSON
+    
+    if nodes != nil
+      nodes_array = codec.decode(nodes)
+      retval = {}
+      
+      nodes_array.each do |node|
+        
+        if Taggable.exists? node
+          t = Taggable.find node
+          retval[node] = t.tags.count  
+        end
+      end
+      
+      render :json => retval
+      end
+  end
+
+  def complexity
+  end
+
+end
