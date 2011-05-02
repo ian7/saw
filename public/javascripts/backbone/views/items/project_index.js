232c01d5ecf1015a16515153d12c094bd4c0d48a
Marcin Nowak <Marcin.Nowak@sonyx.net>
2011-05-02 14:33:08 +0200

    project summary view - inception

--- public/javascripts/backbone/views/items/project_index.js
+++ public/javascripts/backbone/views/items/project_index.js
@@ -4,12 +4,13 @@
 
 App.Views.Items.ProjectItem = Backbone.View.extend({
   events : {
-  },
+	},
 
   alternativesCollection : null,
 
   initialize : function(options) {
 
+	_(this).bindAll('render','alternativesReady','notify');
 	this.alternativesCollection = new Alternatives;
 	
 	this.alternativesCollection.issueView = this;
@@ -17,8 +18,10 @@ App.Views.Items.ProjectItem = Backbone.View.extend({
 	this.alternativesCollection.item_url = this.model.url();
 	this.alternativesCollection.url = this.model.url()+'/alternatives';
 
-	this.alternativesCollection.bind('refresh',this.render);
+	this.alternativesCollection.bind('refresh',this.alternativesReady);
 	
+	// relate alternatives set to the model
+	this.model.alternatives = this.alternativesCollection;
 
     this.render = _.bind(this.render, this); 
     this.model.bind('change', this.render);
@@ -30,31 +33,33 @@ App.Views.Items.ProjectItem = Backbone.View.extend({
 	_(this).bindAll('notify');
 		
 	notifier.register( this );
+	this.alternativesCollection.fetch();
   },
-
+  alternativesReady : function() {
+	this.render();
+  },
   render : function() {
 
-	// fetch alterantives finally !
-	this.alternativesCollection.fetch();
+   this.el.innerHTML = JST.project_show( {item: this.model} );
 
-//	this.tempEL = document.createElement("tr");
-//	this.tempEL.innerHTML = JST.items_index( {item: this.model} );
+/* debug
+   jQuery(this.el).prepend("<div class = 'button red render'>Render!</div>");
+   jQuery(this.el).prepend("<div class = 'button red fetch'>Fetch!</div>");
+*/
 
-   this.el.innerHTML = JST.items_index( {item: this.model} );
-
-//   this.alternativesCollectionView.render();
-
-	
-	// finally attach it ;)
-//	this.el.innerHTML = this.tempEL.innerHTML;
 
    return this;
   },
 
   notify : function( broadcasted_id ) {
 		if( this.model.get('id') == broadcasted_id ) {
-			this.alternativesCollection.fetch();
-			jQuery(this.el).effect("highlight", {}, 500);
+
+			this.model.fetch();
+			
+			// deep refresh is required here to get decision arrays updated
+			this.alternativesCollection.fetch({deepRefresh: true});
+			jQuery(this.el).effect("highlight", {}, 500);	
+
 		}
   }  
 
@@ -62,20 +67,13 @@ App.Views.Items.ProjectItem = Backbone.View.extend({
 
 App.Views.Items.ProjectIndex = Backbone.View.extend({
   events : {
-	"click .newItem" : "newItem",
-	"click .expandAll" : "expandAll",
-	"click .collapseAll" : "collapseAll",
-//	"click .newItem" : 'checkNewItem',
   },
-
-  newItemName : '(new item)',
 
   initialize : function() {
 
-	_(this).bindAll('newItem','checkNewItem','removeNewItem','newItem');
 
-	this.collection.bind('saved',this.checkNewItem );
-	this.collection.bind('refresh',this.checkNewItem );
+//	this.collection.bind('saved',this.checkNewItem );
+//	this.collection.bind('refresh',this.checkNewItem );
 	
 	this.collection.comparator = function( m ) { return m.get('id'); };
 
@@ -102,46 +100,11 @@ App.Views.Items.ProjectIndex = Backbone.View.extend({
 		this._rendered = true;
 		this._itemsCollectionView.el = this.el; 
 		this._itemsCollectionView.render();
-		jQuery(this.el).prepend("<div class = 'button orange collapseAll'>Collapse all</div>");
-		jQuery(this.el).prepend("<div class = 'button orange expandAll'>Expand all</div>");
+//		jQuery(this.el).prepend("<div class = 'button orange collapseAll'>Collapse all</div>");
+//		jQuery(this.el).prepend("<div class = 'button orange expandAll'>Expand all</div>");
 //		jQuery(this.el).prepend("<div class = 'button red newItem'>New!</div>");
-		this.checkNewItem();
+//		this.checkNewItem();
 
-  },
-  removeNewItem : function() {
-		this.collection.each( function( i ) {
-			if( i.get('name') == '(new item)' ) {
-				this.collection.remove( i );
-				delete i;
-			} 
-		},this);
-	
-  },
-  
-  newItem : function() {
-		var collection;
-
-		if( this.collection ) {
-			// we're called from the render method
-			collection = this.collection; 
-		}
-		else {
-			// we're called because collection element has been saved 
-			collection = this;			
-		}
-
-/*		var preLastItem = collection.last();
-		if( preLastItem.view ) {
-			preLastItem.view.expand();
-		}
-*/
-		
-		i = new Item;
-
-		// this.newItemName is unavailable when called by the 'save' event from the collection
-		i.set({name: '(new item)' });
-		collection.add( i );
-		
   },
   notify : function( broadcasted_id ) {
 /*		this.collection.each( function( i ) {	
@@ -166,23 +129,6 @@ App.Views.Items.ProjectIndex = Backbone.View.extend({
 			});
 		}
 		
-  },
-  expandAll : function() {
-	_.each(this._itemsCollectionView._childViews, function( childView ) {
-		childView.expand();
-	});
-  },
-  collapseAll : function() {
-	_.each(this._itemsCollectionView._childViews, function( childView ) {
-		childView.collapse();
-	});
-  },
-  shortcut : function() {
-	// alert("!");
-  },
-  checkNewItem : function() {
-		this.removeNewItem();
-		this.newItem();
   },
 });
 
