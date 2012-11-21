fa77983de42e83d5586bf5f2adaab59cbc538d7e
Marcin Nowak <Marcin.Nowak@sonyx.net>
2012-11-21 20:09:06 +0100

    this one was missing here

---
+++ public/javascripts/backbone/main/modelTag.js
@@ -0,0 +1,17 @@
+/*global Backbone,App,_*/
+
+App.Models.Tag = App.Data.Item.extend({
+    initialize : function( model ){
+      _(this).bindAll();
+
+      App.Models.Project.__super__.initialize.apply(this);
+  
+    },
+   
+});
+
+App.Models.Tags = App.Data.Collection.extend({
+    model: App.Models.Tag,
+    /* default url is set to "projects" root - sub-projects are to be loaded recursively */
+    url: "/tags"
+});
