37ffcab4edf4052119713f541020ebe7cada640b
Marcin Nowak <Marcin.Nowak@sonyx.net>
2012-11-23 18:14:11 +0100

    ribbon clean-up

--- public/javascripts/backbone/main/mainRibbon.js
+++ public/javascripts/backbone/main/mainRibbon.js
@@ -4,12 +4,16 @@ App.module('main',function(){
     this.Views.Ribbon = Backbone.Marionette.ItemView.extend({
         className : 'ribbonWidget',
         tagName : 'div',
-        template : JST['main/mainRibbon'], 
+        template : JST['main/mainRibbon'],
+        events : {
+            'click span#capture' : 'onCapture',
+            'click span#projects' : 'onProjects'
+        },
         initialize : function(){
             // hook up to the routing events
          //   Backbone.history.on('route',this.render,this);
             this.context.listen('project:selected',this.projectSelected);
-            this.context.project.on('change',this.projectChanged,this);
+            this.context.project.on('sync',this.projectChanged,this);
             this.context.on('status',this.statusChanged, this);
             _(this).bindAll();
         },  
@@ -17,13 +21,19 @@ App.module('main',function(){
             //debugger;
         },
         projectSelected : function( options ){
-           jQuery("div#projectID",this.el).html(options.id);
+           jQuery("span#projectID",this.el).html(options.id);
         },
         projectChanged : function(){
-           jQuery("div#projectID",this.el).html(this.context.project.get('name'));
+           jQuery("span#projectID",this.el).html(this.context.project.get('name'));
         },
         statusChanged : function(){
           jQuery("span.status",this.el).html(this.context.status.name);  
+        },
+        onCapture : function(){
+            this.context.dispatchGlobally("capture:issues:list");
+        },
+        onProjects : function(){
+            this.context.dispatchGlobally("projects:index");
         }
     });
 });
