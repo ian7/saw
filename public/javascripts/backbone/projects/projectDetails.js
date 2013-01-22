70f43955fd9675e3e50e37eabb53534a135c3e0d
Marcin Nowak <Marcin.Nowak@sonyx.net>
2013-01-22 15:40:03 +0100

    pieCharting integrated into project status

--- public/javascripts/backbone/projects/projectDetails.js
+++ public/javascripts/backbone/projects/projectDetails.js
@@ -20,9 +20,38 @@ App.module("main.projects",function(){
       this.itemView = App.main.projects.Views.ProjectDetailsIssue;
       this.collection = App.main.capture.context.issues;
       this.itemViewOptions = {context:this.context};
+
+      this.collection.on('decisionsChanged',this.onDecisionsChanged,this);
+
+      this.statusChart = new App.main.Views.PieChart();
     },
     onRender: function(){
+        var chartEl = jQuery("td#graphs",this.el);
+        this.statusChart.setElement( chartEl );
+        this.statusChart.render();
+    },
+    onDecisionsChanged : function(){
+      var decisions = {};
+
+      _(this.collection.models).each( function( issue ){
+        var decisionState = issue.decisionState();
         
+        if( decisions[decisionState]){
+          decisions[decisionState] = decisions[decisionState] + 1;
+        }
+        else{
+          decisions[decisionState] = 1;
+        }
+      },this);
+
+      var decisionArray = [];
+      _(decisions).each( function( decisionCount, decisionDescription ){
+        var decisionRow = [ decisionDescription,decisionCount ];
+        decisionArray.push( decisionRow );
+      },this);
+
+      this.statusChart.data = decisionArray;
+      this.statusChart.render();
     }
   });
 });
