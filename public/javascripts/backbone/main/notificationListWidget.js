6eaa76f6465416842e162c27698378ac357b696d
Marcin Nowak <Marcin.Nowak@sonyx.net>
2013-01-23 00:29:42 +0100

    simple notification sidebar widget

---
+++ public/javascripts/backbone/main/notificationListWidget.js
@@ -0,0 +1,44 @@
+/*global App, Backbone,_,jQuery,JST,eventer*/
+
+App.module("main", function(that, App, Backbone, Marionette, jQuery, _, customArgs) {
+  this.Views.NotificationListWidget = Backbone.Marionette.CompositeView.extend({
+    template: JST['main/notificationListWidget'],
+    itemViewContainer: 'table#notifications tbody',
+    tagName: "div",
+    className: "notificationListWidget",
+    events: {
+    },
+    shortcuts: {},
+    speedButtons: {},
+    initialize: function(options) {
+      _(this).bindAll();
+
+      this.itemView = App.main.Views.NotificationListWidgetItem;
+      this.itemViewOptions = {
+        context: this.context
+      };
+
+      this.collection = new Backbone.Collection();
+
+      eventer.register(this);
+    },
+    onRender: function() {
+    },
+
+    notifyEvent : function( data ) {
+      var notification = JSON.parse(data);
+      
+      // this should stop collection from eliminating duplicates... ugly but works. 
+      notification.itemID = data.id;
+      notification.id = notification.id+(new Date().getTime()).toString();
+
+      var model = new Backbone.Model( notification );
+      this.collection.add(model);
+
+
+      if( this.collection.length > 20 ){
+        this.collection.shift();
+      }
+    }
+  });
+});
