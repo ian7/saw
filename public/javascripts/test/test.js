ddacdfbed22612fd2ecdd59beacae2d5ab6d60e7
Marcin Nowak <Marcin.Nowak@sonyx.net>
2013-03-14 21:15:41 +0100

    this i don't need right now

--- public/javascripts/test/test.js
+++ public/javascripts/test/test.js
@@ -1,18 +1,17 @@
 /*global describe,it*/
-  var Example = window.Example = {};
+/*var Example = window.Example = {};
 
-  Example.truth = function() {
-    return true;
-  };
-
-
+Example.truth = function() {
+  return true;
+};
 
 
 
-  describe('Example', function() {
-    return describe('#truth()', function() {
-      return it('should return Boolean true', function() {
-        return Example.truth().should.be["true"];
-      });
+describe('Example', function() {
+  return describe('#truth()', function() {
+    return it('should return Boolean true', function() {
+      return Example.truth().should.be["true"];
     });
   });
+});
+*/
