37b8562ffb6b97da8879cf7a9ef80c8d1e072adf
Marcin Nowak <Marcin.Nowak@sonyx.net>
2013-03-26 21:24:31 +0100

    more tests revealing nothing

--- public/javascripts/test/main/model.project.js
+++ public/javascripts/test/main/model.project.js
@@ -46,26 +46,48 @@ describe('Project', function() {
         });
     });
 
-    it('removes issue after it is deleted', function( done ){
-        newIssue.destroy();
+    var tempProjectID = null;
 
-        captureContext.issues.on('remove', function(){
-            done();
-        });
-    });
-
-   /* it('loads issues for the extsting project', function( done ){
+    it('loads issues for the extsting project', function( done ){
         this.timeout(10000);
-        Project.set('id','4fb0d1fa924ff85581000003');
-        Project.fetch();
+        tempProjectID = Project.get('id');
+        
+        Project.set('id','4f9d830ae90a3e203400000b');
 
         captureContext.issues.on('add',function(){
-            done();
+            if( captureContext.issues.length === 9 ) {
+                done();
+            }
         });
-
+        Project.fetch();
     });
 
-    it('should clean up nicely after destroying Project', function( done ){
+    it('loads back the temporary project', function( done ){
+        this.timeout(10000);
+        Project.set('id',tempProjectID);
+
+        var handler = function(){
+            if( captureContext.issues.length === 1 ) {
+                done();
+            }
+        };
+        captureContext.issues.on('add',handler);
+        captureContext.issues.on('remove',handler);
+
+        Project.fetch();
+    });
+
+   /* it('removes issue after it is deleted', function( done ){
+
+        captureContext.issues.on('remove', function(){
+            if( captureContext.issues.length === 0 ){
+                done();
+            }
+        });
+        newIssue.destroy();
+    });*/
+
+   /* it('should clean up nicely after destroying Project', function( done ){
         this.timeout(10000);
         Project.destroy({success: function(){
             done();
