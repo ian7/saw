2a08defddf06946ffada7fe932e3bee4ce19ebd9
Marcin Nowak <Marcin.Nowak@sonyx.net>
2013-04-08 11:58:33 +0200

    issue status refresh on render...

--- public/javascripts/backbone/capture/issueListItem.js
+++ public/javascripts/backbone/capture/issueListItem.js
@@ -63,6 +63,7 @@ App.module("main.capture",function(){
         this.focus();
         console.log("focusing");
       }
+      this.onDecisionsChanged();
     },
     onDecisionsChanged : function(){
         jQuery('span#issueStatus',this.el).html( this.model.decisionState() );
