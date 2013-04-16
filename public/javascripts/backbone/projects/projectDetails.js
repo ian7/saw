aa661cf8d8450a208c40c85ca6e1c56b4be16143
thebouh <evgenij.r@gmail.com>
2013-04-16 19:33:20 +0200

    changed PieChart to Alternatives decisions.

--- public/javascripts/backbone/projects/projectDetails.js
+++ public/javascripts/backbone/projects/projectDetails.js
@@ -27,31 +27,36 @@ App.module("main.projects",function(){
     },
     onRender: function(){
         var chartEl = jQuery("td#graphs",this.el);
+  //      jQuery("div#nameOfIssue",this.el).html( App.main.capture.context.issues.models[0].attributes.name);
         this.statusChart.setElement( chartEl );
         this.statusChart.render();
         this.onDecisionsChanged();
     },
     onDecisionsChanged : function(){
+      
       var decisions = {};
-
-      _(this.collection.models).each( function( issue ){
-        var decisionState = issue.decisionState();
-        
-        if( decisions[decisionState]){
-          decisions[decisionState] = decisions[decisionState] + 1;
+	  var alternativesDecision = [];
+	  var a = App.main.capture.context.issues.models[0].alternatives;
+	  
+	  for (var i = 0; i < a.length; i++){
+	  	var decisionDesc = (a.models[i].isDecided()) ? a.models[i].decision() : a.models[i].isColliding() ? "Colliding" : "Not decided"; 
+	  	if( decisions[decisionDesc]){
+          decisions[decisionDesc] = decisions[decisionDesc] + 1;
         }
         else{
-          decisions[decisionState] = 1;
+          decisions[decisionDesc] = 1;
         }
-      },this);
+	  }
 
-      var decisionArray = [];
-      _(decisions).each( function( decisionCount, decisionDescription ){
-        var decisionRow = [ decisionDescription,decisionCount ];
-        decisionArray.push( decisionRow );
-      },this);
-
-      this.statusChart.data = decisionArray;
+//	  console.log(decisions)	  
+	  alternativesDecision.push(["Open", decisions["4f9d67e2e90a3e18b900002e"]])
+	  alternativesDecision.push(["Positive", decisions["4f9d67e2e90a3e18b900002c"]])
+	  alternativesDecision.push(["Negative", decisions["4f9d67e2e90a3e18b900002d"]])
+	  alternativesDecision.push(["Colliding", decisions["Colliding"]])
+	  alternativesDecision.push(["Not decided", decisions["Not decided"]])
+			
+      this.statusChart.data = alternativesDecision;
+      this.statusChart.options.title =  App.main.capture.context.issues.models[0].attributes.name;
       this.statusChart.render();
     }
   });
