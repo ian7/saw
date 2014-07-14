c92a86f2f11b477976408d1d3ca75a7b4cb831ea
Marcin Nowak <Marcin.Nowak@sonyx.net>
2014-07-14 14:28:21 +0200

    some more stats on M1, project mapper moved to the main script

--- analysis/indicators.rb
+++ analysis/indicators.rb
@@ -16,18 +16,82 @@ class Class
  end
 end
 
+
+class ProjectMapper
+	def initialize
+		@projects = {
+			"ex4" => "Run 1",
+			"ex5" => "Run 2",
+			"ex6" => "Exercise 6",
+			"ex7" => "Run 3",
+			"ex8" => "Run 4",
+			"ex9" => "Run 5",
+			"517652dfda300c1675000001" => "Exercise 3",
+			"51765a68da300c1849000001" => "Run 1",
+			"5187d355da300c37f7000001" => "Run 2",
+			"518a833eda300c484c000001" => "Exercise 6",
+			"51925958da300c697d000001" => "Run 3",
+			"5193a637da300c3002000001" => "Run 4",
+			"519a3a22da300c3793000001" => "Run 5"
+		}
+
+		@exclusions = [
+			"Exercise 3",
+			"Exercise 6",
+		]
+
+=begin
+		@projects = {
+			"ex4" => "Exercise 4",
+			"ex5" => "Exercise 5",
+			"ex6" => "Exercise 6",
+			"ex7" => "Exercise 7",
+			"ex8" => "Exercise 8",
+			"ex9" => "Exercise 9",
+			"517652dfda300c1675000001" => "Exercise 3",
+			"51765a68da300c1849000001" => "Exercise 4",
+			"5187d355da300c37f7000001" => "Exercise 5",
+			"518a833eda300c484c000001" => "Exercise 6",
+			"51925958da300c697d000001" => "Exercise 7",
+			"5193a637da300c3002000001" => "Exercise 8",
+			"519a3a22da300c3793000001" => "Exercise 9"
+		}
+=end
+	end
+	def [](id)
+		value = @projects.find { |key, value| key == id}
+		if value
+			return value[1]
+		else
+			return ""
+		end
+	end
+	def isExcluded( projectId )
+		@exclusions.index(self[projectId])
+	end
+end
+
+
+
+
 require './analysis/toolkit/indicators.rb'
 require './analysis/toolkit/metricMatrix.rb'
 
 outputPath = "./analysis/output/"
 
+pm = ProjectMapper.new
 allProjects = MetricMatrix.new
 
 allIssues = MetricMatrix.new
 allIssues.load("issues.mm")
+allIssues = allIssues.select{ |issue| !pm.isExcluded(issue["Project"] ) }.select{|issue| issue["Destroyed"] == "0"}
+
 
 allAlternatives = MetricMatrix.new
 allAlternatives.load("alternatives.mm")
+allAlternatives = allAlternatives.select{ |issue| !pm.isExcluded(issue["Project"] ) }.select{|issue| issue["Destroyed"] == "0"}
+
+#debugger
 
 Indicator.subclasses.each { |indicator|
 	i = indicator.new( allProjects, allIssues, allAlternatives)
--- analysis/toolkit/indicators/M1.rb
+++ analysis/toolkit/indicators/M1.rb
@@ -1,32 +1,5 @@
 require_relative './indicator.rb'
 
-class ProjectMapper
-	def initialize
-		@projects = {
-			"ex4" => "Exercise 4",
-			"ex5" => "Exercise 5",
-			"ex6" => "Exercise 6",
-			"ex7" => "Exercise 7",
-			"ex8" => "Exercise 8",
-			"ex9" => "Exercise 9",
-			"517652dfda300c1675000001" => "Exercise 3",
-			"51765a68da300c1849000001" => "Exercise 4",
-			"5187d355da300c37f7000001" => "Exercise 5",
-			"518a833eda300c484c000001" => "Exercise 6",
-			"51925958da300c697d000001" => "Exercise 7",
-			"5193a637da300c3002000001" => "Exercise 8",
-			"519a3a22da300c3793000001" => "Exercise 9"
-		}
-	end
-	def [](id)
-		value = @projects.find { |key, value| key == id}
-		if value
-			return value[1]
-		else
-			return ""
-		end
-	end
-end
 
 
 class M1 < Indicator
@@ -52,6 +25,9 @@ class M1 < Indicator
 
 		pm = ProjectMapper.new
 
+		issueCountsEP = []
+		issueCoutnsSAW = []
+
 		projects = @issues.uniq {|x| pm[x['Project']] } .map{|x| pm[x["Project"]] }
 		projects.sort!
 		projects.each{ |project|
@@ -75,9 +51,18 @@ class M1 < Indicator
 			if exclusions.find {|x| x==project}
 				#do nothing
 			else
+				issueCountsEP << projectIssues.select{|x| x["ID"].length <= 7}.length
+				issueCoutnsSAW << projectIssues.select{|x| x["ID"].length > 7}.length
 				out << lineOut
 			end
 		}
+
+		# EP
+		#debugger
+		puts "EP total: " + issueCountsEP.inject(:+).to_s + " avg: " + issueCountsEP.mean.to_s + " sd: " + issueCountsEP.standard_deviation.to_s
+		puts "SAW total: " + issueCoutnsSAW.inject(:+).to_s + " avg: " + issueCoutnsSAW.mean.to_s + " sd: " + issueCoutnsSAW.standard_deviation.to_s
+
+
 		return out
 	end
 end
